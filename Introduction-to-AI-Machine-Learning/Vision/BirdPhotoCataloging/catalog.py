536cc5d0cb277149abfe55d53270086c486ba20f
R3J3NT <13350956+R3J3NT@users.noreply.github.com>
2020-12-05 17:58:06 +0100

    Add function to call Custom Vision API with photo from device

--- Introduction-to-AI-Machine-Learning/Vision/BirdPhotoCataloging/catalog.py
+++ Introduction-to-AI-Machine-Learning/Vision/BirdPhotoCataloging/catalog.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import requests
+from PIL import Image
+from io import BytesIO
 
 subscription_key = ""
 endpoint = ""
@@ -9,7 +11,7 @@ def load_env_variables():
 
     global subscription_key
     global endpoint
-
+    
     if 'SUBSCRIPTION_KEY' in os.environ:
         subscription_key = os.environ['SUBSCRIPTION_KEY']
     else:
@@ -21,7 +23,23 @@ def load_env_variables():
     else:
         print('ERROR - Set ENDPOINT environment variable')
         sys.exit()
-        
+
+def call_custom_vision_API(path):
+
+    analyze_image_data = open(path, "rb").read()
+    headers = {'Prediction-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
+    params = {}
+    response = requests.post(endpoint, headers=headers, params=params, data = analyze_image_data)
+    response.raise_for_status()
+    analysis = response.json()
+
+    return analysis
+
 load_env_variables()
 print(subscription_key)
 print(endpoint)
+
+analyze_image_path = r''
+
+analysis = call_custom_vision_API(analyze_image_path)
+print(str(analysis))

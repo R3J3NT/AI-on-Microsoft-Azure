d4e0258e87024adabf31d0a095a1e2ac226c4456
R3J3NT <13350956+R3J3NT@users.noreply.github.com>
2020-12-05 17:35:38 +0100

    Add reading env variables due to connection with Azure Custom Vision API

--- Introduction-to-AI-Machine-Learning/Vision/BirdPhotoCataloging/catalog.py
+++ Introduction-to-AI-Machine-Learning/Vision/BirdPhotoCataloging/catalog.py
@@ -1,1 +1,27 @@
-s
+import os
+import sys
+import requests
+
+subscription_key = ""
+endpoint = ""
+
+def load_env_variables():
+
+    global subscription_key
+    global endpoint
+
+    if 'SUBSCRIPTION_KEY' in os.environ:
+        subscription_key = os.environ['SUBSCRIPTION_KEY']
+    else:
+        print('ERROR - Set SUBSCRIPTION_KEY environment variable')
+        sys.exit()
+
+    if 'ENDPOINT' in os.environ:
+        endpoint = os.environ['ENDPOINT']
+    else:
+        print('ERROR - Set ENDPOINT environment variable')
+        sys.exit()
+        
+load_env_variables()
+print(subscription_key)
+print(endpoint)
